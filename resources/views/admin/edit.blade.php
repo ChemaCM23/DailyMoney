f54614b0ec54314c063ea9e20ef59cc2959cbea1
ChemaCM23 <josemariacm23@gmail.com>
2024-06-18 21:15:36 +0200

    fixed

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -1,33 +1,33 @@
 <x-app-layout>
 
-    <div class="max-w-md mx-auto p-6 fondoUser overflow-hidden shadow-sm sm:rounded-lg mt-10">
+    <div class="max-w-md mx-auto p-6 fondoUser overflow-hidden shadow-sm sm:rounded-lg mt-10 mb-10">
         <form method="post" action="{{ route('admin.update', ['admin' => $user->id]) }}" class="space-y-6">
             @csrf
             @method('PUT')
             <div class=" text-center">
-                <h2 class="text-3xl font-bold text-black">Editor de Perfil</h2>
+                <h2 class="text-3xl font-bold text-white">Editor de Perfil</h2>
             </div>
             <div class="border rounded-md p-4">
                 <div >
-                    <label for="name" class="block text-sm font-medium  dark:text-black">Nombre</label>
+                    <label for="name" class="block text-sm font-medium  text-white">Nombre</label>
                     <x-text-input id="name" name="name" type="text" class="mt-1 block w-full rounded-md" :value="old('name', $user->name)" required autofocus autocomplete="name" />
                     <x-input-error class="mt-2" :messages="$errors->get('name')" />
                 </div>
 
                 <div class="mt-6">
-                    <label for="surname" class="block text-sm font-medium  dark:text-black">Apellido</label>
+                    <label for="surname" class="block text-sm font-medium  text-white">Apellido</label>
                     <x-text-input id="surname" name="surname" type="text" class="mt-1 block w-full rounded-md" :value="old('surname', $user->surname)" required autofocus autocomplete="surname" />
                     <x-input-error class="mt-2" :messages="$errors->get('surname')" />
                 </div>
 
                 <div class="mt-6">
-                    <label for="phone" class="block text-sm font-medium  dark:text-black">Teléfono</label>
+                    <label for="phone" class="block text-sm font-medium  text-white">Teléfono</label>
                     <x-text-input id="phone" name="phone" type="text" class="mt-1 block w-full rounded-md" :value="old('phone', $user->phone)" required autofocus autocomplete="phone" />
                     <x-input-error class="mt-2" :messages="$errors->get('phone')" />
                 </div>
 
                 <div class="mt-6">
-                    <label for="email" class="block text-sm font-medium  dark:text-black">Email</label>
+                    <label for="email" class="block text-sm font-medium  text-white">Email</label>
                     <x-text-input id="email" name="email" type="email" class="mt-1 block w-full rounded-md" :value="old('email', $user->email)" required autocomplete="username" />
                     <x-input-error class="mt-2" :messages="$errors->get('email')" />
                 </div>
@@ -54,10 +54,13 @@
         </form>
     </div>
 
+    <x-footer></x-footer>
+
 </x-app-layout>
 
 <style>
     .fondoUser {
-        background: radial-gradient(#b6b601ba, #62680e);
+        background: radial-gradient(#7e00ac, #3e0065);
     }
+
 </style>
